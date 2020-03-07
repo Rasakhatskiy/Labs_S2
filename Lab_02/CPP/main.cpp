879d856fa51c7c638f20e029d4a613a8809518f2
Rasakhatskiy <saharok.m@yandex.ua>
2020-03-07 17:19:48 +0200

    Lab 2 insert

--- Lab_02/CPP/main.cpp
+++ Lab_02/CPP/main.cpp
@@ -4,5 +4,10 @@
 int main()
 {
 	List<int> list = List<int>();
+	list.Insert(13, 0);
+	list.Insert(12, 0);
+	list.Insert(14, 1);
+	list.Insert(15, 2);
+	//12 14 15 13 
 	//list.CreateEmpty();
 }
--- Lab_02/Include/List.hpp
+++ Lab_02/Include/List.hpp
@@ -1,14 +1,16 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <stdexcept>
+
 template <typename Type>
 class List
 {
 public:
-	//Does nothing, because i see no difference between constructor and create_empty
+	//calls create empty
 	List<Type>();
 
-	//Deletes *array[]
+	//
 	~List();
 	
 	//Creates an empty list or just clears current
@@ -29,32 +31,138 @@ public:
 	//Replaces element at index with given element
 	void Set(Type element, int index);
 
+	bool IsError();
+
 private:
 	struct Node
 	{
 		Type Value;
 		Node* Next;
-		Node* Previous;
+
+		Node(Type value, Node* next)
+		{
+			Value = value;
+			Next = next;
+		}
+
+		Node() = default;
 	};
+
+	Node* Root;
 
 	//minimal real size of dynamic array
 	const int MIN_REAL_SIZE = 16;
+
+	//true if error ocurred
+	bool Error = false;
 
 	//Real size of dynamic array
 	int RealSize;
 
 	//Shown size to user
 	int Size;
-
-	//Dynamic array of objects
-	Type* Array[];
-
-	//Increases array size by value
-	void Upgrade(int size);
-
-	//Decreases array size by value
-	void Downgrade(int size);
 };
 
+template <typename Type>
+void List<Type>::CreateEmpty()
+{
+	Size = 0;
+	RealSize = MIN_REAL_SIZE;
+	Root = new Node();
+	Root->Next = Root;
+}
+
+template<typename Type>
+int List<Type>::Length()
+{
+	return Size;
+}
+
+template<typename Type>
+void List<Type>::Insert(Type value, int index)
+{
+	try 
+	{
+		if (index > Size || index < 0)
+		{
+			throw std::out_of_range("Index out of range");
+		}
+		else
+		{
+			if (Size == 0)
+			{
+				Root->Value = value;
+				Size++;
+				return;
+			}
+			if (index == 0)
+			{
+				Node* newRoot = new Node(value, Root);
+				Node* temp = Root;
+				while (temp->Next != Root)
+					temp = temp->Next;
+				temp->Next = newRoot;
+				Root = newRoot;
+			}
+			else
+			{
+				int count = 1;
+				Node* prev = Root;
+				while (count != index)
+				{
+					prev = prev->Next;
+					count++;
+				}
+				Node* temp = new Node(value, prev->Next);
+				prev->Next = temp;
+			}
+			Size++;
+		}
+	}
+	catch (...)
+	{
+		Error = true;
+	}
+
+	return;
+}
+
+template<typename Type>
+void List<Type>::Remove(int index)
+{
+	//todo
+}
+
+template<typename Type>
+Type List<Type>::Get(int index)
+{
+	//todo
+	return Type();
+}
+
+template<typename Type>
+void List<Type>::Set(Type element, int index)
+{
+	//todo
+}
+
+
+template<typename Type>
+bool List<Type>::IsError()
+{
+	return Error;
+}
+
+
+template <typename Type>
+List<Type>::List()
+{
+	CreateEmpty();
+}
+
+template<typename Type>
+List<Type>::~List()
+{
+}
 #endif // !LIST_HPP
 
