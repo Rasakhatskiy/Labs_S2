f340b4cba8bd8cbd302d751fedcbf937f26271c1
Rasakhatskiy <saharok.m@yandex.ua>
2020-03-27 17:12:44 +0200

    Lab_03(a) Quick sort

--- Lab_03/CPP/main.cpp
+++ Lab_03/CPP/main.cpp
@@ -8,24 +8,36 @@ void PrintArray(Point3D arr[], int size)
 	{
 		std::cout << "(" << arr[i].X << " " << arr[i].Y << " " << arr[i].Z << ")" << std::endl;
 	}
+	std::cout << std::endl;
 }
 
 
 int main()
 {
-	Point3D p1(1, 2, 3);
-	Point3D p2(1, 2, 4);
+	Point3D p0(1, 1, 1);
+	Point3D p1(2, 3, 5);
+	Point3D p2(2, 2, 4);
 	Point3D p3(1, 3, 4);
 	Point3D p4(1, 3, 5);
+	Point3D p5(3, 1, 4);
+	Point3D p6(1, 5, 9);
+	Point3D p7(2, 6, 5);
+	Point3D p8(7, 7, 7);
+	Point3D p9(2, 2, 2);
 
-	Point3D arr[8];
-	arr[0] = p1;
-	arr[1] = p4;
-	arr[2] = p3;
-	arr[3] = p2;
+	const int size = 10;
 
-	InsertionSort(arr, 4);
 
-	p1 = p2;
-	p2 = p3;
+	Point3D arr[size] = { p0, p1, p2, p3, p4, p5, p6, p7, p8, p9};
+	PrintArray(arr, size);
+	InsertionSort(arr, 0, size - 1);
+	PrintArray(arr, size);
+
+
+	Point3D arr2[size] = { p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 };
+	PrintArray(arr2, size);
+	QuickSort(arr2, 0, size - 1);
+	PrintArray(arr2, size);
+
+
 }
--- Lab_03/Include/Sort.hpp
+++ Lab_03/Include/Sort.hpp
@@ -1,18 +1,20 @@
 #ifndef SORT_HPP
 #define SORT_HPP
 #include "../Include/Point.hpp"
+//---------------------------------------INSERTION-------------------------------------
 
-void InsertionSort(Point3D arr[], int size)
+
+void InsertionSort(Point3D arr[], int lowest, int highest)
 {
 	Point3D comparer;
 	int i, j;
-	for (i = 1; i < size; i++)
+	for (i = lowest + 1; i <= highest; i++)
 	{
 		comparer = arr[i];
 		j = i - 1;
 		
 		//if element is smaller than comparer, move it one position forward
-		while (j >= 0 && arr[j] < comparer)
+		while (j >= lowest && arr[j] < comparer)
 		{
 			arr[j + 1] = arr[j];
 			j = j - 1;
@@ -21,4 +23,50 @@ void InsertionSort(Point3D arr[], int size)
 	}
 }
 
-#endif //INSERTION_SORT_HPP
+//---------------------------------------QUICK SORT------------------------------------
+
+int FindPartitionIndex(Point3D arr[], int lowest, int highest)
+{
+	Point3D pivot = arr[lowest];
+	int i = lowest - 1;
+	int j = highest + 1;
+
+	while (true)
+	{
+		//find index before leftmost >= pivot 
+		do i++;
+		while (arr[i] > pivot);
+
+		//find index before rightmost <= pivot
+		do j--;
+		while (arr[j] < pivot);
+
+		//if indexes are the same return it 
+		if (i >= j) return j;
+
+		//else swap it
+		Point3D temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
+
+//Несквик сорт за спаданням
+void QuickSort(Point3D arr[], int lowest, int highest)
+{
+	if (lowest < highest)
+	{
+		int partitionIndex = FindPartitionIndex(arr, lowest, highest);
+
+		//sort part of array before partition index
+		QuickSort(arr, lowest, partitionIndex);
+
+		//sort part of array after partition index
+		QuickSort(arr, partitionIndex + 1, highest);
+	}
+}
+
+
+
+
+#endif //SORT_HPP
